acb337e3cbf7ea53e29f1f364ee20004806c5d56
Rhio Adjie Fabian <rhioadjie20@gmail.com>
2021-12-07 11:15:57 +0700

    First commit

---
+++ config-server/src/test/java/com/xtremax/mcs/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xtremax.mcs.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ reservation-services/src/test/java/com/xtremax/mcs/reservationservices/ReservationServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xtremax.mcs.reservationservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
